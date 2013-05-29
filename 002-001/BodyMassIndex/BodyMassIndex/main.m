7857661ea80939848dd6bc79c39458c88d53386a
Erik van der Neut <vanderneut@gmail.com>
2013-05-29 11:26:14 -0400

    Converting tab indentation back to spaces indentation -- for now. Tab indentation not showing well in Github :-(

--- 002-001/BodyMassIndex/BodyMassIndex/main.m
+++ 002-001/BodyMassIndex/BodyMassIndex/main.m
@@ -20,7 +20,7 @@ NSValue * createMetrics(float weightInLbs, float heightInInches)
     struct BodyMetrics metrics;
     metrics.weight = weightInLbs;
     metrics.height = heightInInches;
-    
+
     return [NSValue valueWithBytes:&metrics objCType:@encode(struct BodyMetrics)];
 }
 
@@ -30,26 +30,26 @@ int main(int argc, const char * argv[])
     {
         // Set up the data:
         NSArray *cases = [NSMutableArray arrayWithObjects:
-                                createMetrics(260.0, 73.0),
-                                createMetrics(289.0, 71.0),
-                                createMetrics( 90.0, 61.0),
-                                createMetrics(105.0, 59.0),
-                                nil];
+                                            createMetrics(260.0, 73.0),
+                                            createMetrics(289.0, 71.0),
+                                            createMetrics( 90.0, 61.0),
+                                            createMetrics(105.0, 59.0),
+                                            nil];
 
         // Loop through the data and calculate and log BMI for each person:
         NSUInteger numberOfCases = [cases count];
         for (int i = 0; i < numberOfCases; i++)
         {
-			// Retrieve metrics structure from the NSValue in the NSArray:
-			NSValue *metricsValue = cases[i];
-			struct BodyMetrics metrics;
-			[metricsValue getValue:&metrics];
-			
-			// Get lbs and inches for easier reference:
-			float lbs = metrics.weight;
-			float inches = metrics.height;
-			
-			// Calculate BMI, create height string, and log for this person:
+            // Retrieve metrics structure from the NSValue in the NSArray:
+            NSValue *metricsValue = cases[i];
+            struct BodyMetrics metrics;
+            [metricsValue getValue:&metrics];
+
+            // Get lbs and inches for easier reference:
+            float lbs = metrics.weight;
+            float inches = metrics.height;
+
+            // Calculate BMI, create height string, and log for this person:
             NSNumber *bmi = getBMI(getKg(lbs), getCm(inches));
             NSString *height = getHeight(inches);
             NSLog(@"Case %i: BMI is %.1f (%.0f lbs, %@)", i, [bmi floatValue], lbs, height);
