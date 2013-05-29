2462f39bade18043d1fd3c026c7c67b66dee94a9
Erik van der Neut <vanderneut@gmail.com>
2013-05-29 11:15:46 -0400

    Week 1, Lesson 2: Data Types, Functions and Expressions -- This version of the BMI app logs the BMI for four different people, looping through an NSArray of NSValue objects that each hold a BodyMetrics strut. This is the log output:
    2013-05-29 11:13:16.678 BodyMassIndex[92520:303] Case 0: BMI is 34.3 (260 lbs, 6'1")
    2013-05-29 11:13:16.683 BodyMassIndex[92520:303] Case 1: BMI is 40.3 (289 lbs, 5'11")
    2013-05-29 11:13:16.684 BodyMassIndex[92520:303] Case 2: BMI is 17.0 (90 lbs, 5'1")
    2013-05-29 11:13:16.684 BodyMassIndex[92520:303] Case 3: BMI is 21.2 (105 lbs, 4'11")
    
    Erik

--- 002-001/BodyMassIndex/BodyMassIndex/main.m
+++ 002-001/BodyMassIndex/BodyMassIndex/main.m
@@ -9,20 +9,51 @@
 #import <Foundation/Foundation.h>
 #import "Conversions.h"
 
+struct BodyMetrics
+{
+    float weight;   // person's weight in lbs
+    float height;   // person's height in inches
+};
+
+NSValue * createMetrics(float weightInLbs, float heightInInches)
+{
+    struct BodyMetrics metrics;
+    metrics.weight = weightInLbs;
+    metrics.height = heightInInches;
+    
+    return [NSValue valueWithBytes:&metrics objCType:@encode(struct BodyMetrics)];
+}
+
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool
     {
         // Set up the data:
-        int id = 1;
-        float lbs = 260.0;
-        float inches = 73.0;
-        
+        NSArray *cases = [NSMutableArray arrayWithObjects:
+                                createMetrics(260.0, 73.0),
+                                createMetrics(289.0, 71.0),
+                                createMetrics( 90.0, 61.0),
+                                createMetrics(105.0, 59.0),
+                                nil];
+
         // Loop through the data and calculate and log BMI for each person:
-        NSNumber *bmi = getBMI(getKg(lbs), getCm(inches));
-        NSString *height = getHeight(inches);
-        NSLog(@"Case %i: BMI is %@ (%f lbs, %@)", id, bmi, lbs, height);
+        NSUInteger numberOfCases = [cases count];
+        for (int i = 0; i < numberOfCases; i++)
+        {
+			// Retrieve metrics structure from the NSValue in the NSArray:
+			NSValue *metricsValue = cases[i];
+			struct BodyMetrics metrics;
+			[metricsValue getValue:&metrics];
+			
+			// Get lbs and inches for easier reference:
+			float lbs = metrics.weight;
+			float inches = metrics.height;
+			
+			// Calculate BMI, create height string, and log for this person:
+            NSNumber *bmi = getBMI(getKg(lbs), getCm(inches));
+            NSString *height = getHeight(inches);
+            NSLog(@"Case %i: BMI is %.1f (%.0f lbs, %@)", i, [bmi floatValue], lbs, height);
+        }
     }
     return 0;
 }
