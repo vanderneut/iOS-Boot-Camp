f62f3f7c13f9797e4edc53cbdc0012a06f4fda0c
Erik van der Neut <vanderneut@gmail.com>
2013-06-09 13:32:37 -0400

    Week 1, Lesson 5: Advanced Class Features: Completed exercise 5; added the Delegate and experimented with a custom Category to see the effect of that. -- Erik

---
+++ 005-001/BMI/DataHandler.h
@@ -0,0 +1,13 @@
+//
+//  DataHandler.h
+//  BMI
+//
+//  Created by Erik van der Neut on 6/8/13.
+//  Copyright (c) 2013 Matthew Campbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataHandler : NSObject
+
+@end
---
+++ 005-001/BMI/DataHandler.m
@@ -0,0 +1,13 @@
+//
+//  DataHandler.m
+//  BMI
+//
+//  Created by Erik van der Neut on 6/8/13.
+//  Copyright (c) 2013 Matthew Campbell. All rights reserved.
+//
+
+#import "DataHandler.h"
+
+@implementation DataHandler
+
+@end
